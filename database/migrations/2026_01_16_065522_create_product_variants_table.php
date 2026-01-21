7484c4e34932482647e7da4db4b5d4a5cfb6796c
AlMahfuzKhanD <almahfuz380@gmail.com>
2026-01-21 15:12:31 +0600

    migration work done

--- database/migrations/2026_01_16_065522_create_product_variants_table.php
+++ database/migrations/2026_01_16_065522_create_product_variants_table.php
@@ -6,27 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::dropIfExists('product_variants');
         Schema::create('product_variants', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
-            $table->string('size')->nullable(); // e.g. M, L, XL, 42, 44
-            $table->string('color')->nullable(); // e.g. Red, Blue
+
+            $table->foreignId('product_id')
+                  ->constrained('products')
+                  ->cascadeOnDelete();
+
+            $table->string('size')->nullable(); // M, L, XL, 42, 44
+            $table->string('color')->nullable(); // Red, Blue
             $table->string('sku')->unique()->nullable();
+
             $table->integer('stock_quantity')->default(0);
-            $table->decimal('extra_price', 10, 2)->default(0); // Price added to base product price
+            $table->decimal('extra_price', 10, 2)->default(0);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('product_variants');
